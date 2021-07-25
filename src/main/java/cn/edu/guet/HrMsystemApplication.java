dd08cf1a5fea53a1e6affab38a2a96f94e9c6c5a
pjy <450857746@qq.com>
2021-07-25 14:25:33 +0800

    增删改查

--- src/main/java/cn/edu/guet/HrMsystemApplication.java
+++ src/main/java/cn/edu/guet/HrMsystemApplication.java
@@ -3,9 +3,11 @@ package cn.edu.guet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("cn.edu.guet.mapper")
+@ComponentScan(basePackages = "cn.edu.guet.*")
 public class HrMsystemApplication {
 
     public static void main(String[] args) {
